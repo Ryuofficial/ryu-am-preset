9012b055963a00ef8cdf865124ad3a168449078f
XXXRAINIER09 <rainiersison123@gmail.com>
2024-07-10 14:22:20 +0800

    fix nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -13,22 +13,22 @@ function Nav() {
           <div className=" z-10 border border-t-[0px] border-r-[0px] border-l-[0px] border-[#151722] md:border-none text-[#bebebe] nav-links-bar  duration-500 md:static absolute bg-[#0e0f16] py-10 md:py-0 md:min-h-fit min-h-min left-0 top-[-100%] md:w-auto w-full flex item-center px-10">
             <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
               <li>
-                <a className="hover:text-[#f81b3c]" href="#About">
+                <a className="hover:text-[#2af598]" href="#About">
                   About
                 </a>
               </li>
               <li>
-                <a className="hover:text-[#f81b3c]" href="#features">
+                <a className="hover:text-[#2af598]" href="#features">
                   Features
                 </a>
               </li>
               <li>
-                <a className="hover:text-[#f81b3c]" href="#faqs">
+                <a className="hover:text-[#2af598]" href="#faqs">
                   Faqs
                 </a>
               </li>
               <li>
-                <a className="hover:text-[#f81b3c]" href="#Contact-section">
+                <a className="hover:text-[#2af598]" href="#Contact-section">
                   Contact
                 </a>
               </li>
@@ -36,7 +36,7 @@ function Nav() {
           </div>
 
           <div className="flex items-center gap-6">
-            {/* <button className="bg-[#a6c1ee] text-[#f81b3c] px-5 py-2 rounded-full hover:bg-[#87acec]">
+            {/* <button className="bg-[#a6c1ee] text-[#2af598] px-5 py-2 rounded-full hover:bg-[#87acec]">
             Sign In
           </button> */}
 
